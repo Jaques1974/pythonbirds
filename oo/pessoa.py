30d108aff03f8eeaa47e1b9fcfc7c00ecf512027
jaques@1974 <jaquesaurelio.74@gmail.com>
2019-10-16 17:29:18 -0300

    Criado e removido atributo dinâmico de objeto do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,25 @@
 class Pessoa:
-    def __init__(self, *filhos, nome = None, idade = 35):
-        self.idade = idade
-        self.nome = nome
-        self.filhos = list(filhos)
-
-    def cumprimentar(self):
-        return f'Ola {id(self)}'
+    def __init__(self, *filhos, nome = None, idade = 35):#---->Aqui são parâmetros(None,35)para os valores(nome, idade).
+        self.idade = idade  #----> Existem também os atributos de dados(que podem ser chamados também atributos de obje-
+        self.nome = nome          #to) que são definidos pelo Método 'init'.Para criar o atributo de objeto, colocamos
+        self.filhos = list(filhos) #'self' mais o ponto e seguido do nome do atributo.O que vem depois do sinal de igual
+                                   # é o valor do atributo(aqui no:self.ATRIBUTO = VALOR)
+    def cumprimentar(self):   #--->'cumprimentar' aqui, é chamado de Método(que definimos chamando a função 'def'), que
+        return f'Ola {id(self)}'  #é uma espécie de atributo da classe Pessoa.
 
 
 if __name__ == '__main__':
-    renzo = Pessoa(nome='Renz0')
-    luciano= Pessoa(renzo, nome='Luciano')
-    print(Pessoa.cumprimentar(luciano))
-    print(id(luciano))
+    renzo = Pessoa(nome='Renzo') #--->Os filhos do objeto 'luciano' é um atributo complexo.Então para sermos bem especí-
+    luciano= Pessoa(renzo, nome='Luciano') #ficos, já onde tinhamos a criação do objeto(p = Pessoa('luciano'),agora em
+    print(Pessoa.cumprimentar(luciano)) #vez de 'p', vamos chamar o objeto de 'luciano' e entre parênteses onde tinha-
+    print(id(luciano)) #mos o parâmetro 'luciano', vamos colocar primeiro o valor 'nome' seguido do parâmetro 'luciano'.
     print(luciano.cumprimentar())
     print(luciano.nome)
     print(luciano.idade)
-    for filho in luciano.filhos:
-        print(filho.nome)
+    for filho in luciano.filhos:   #O que está escrito no for, se lê assim: Para cada filho dos filhos de luciano, im-
+        print(filho.nome)          #prima o nome do filho.
+    luciano.sobrenome = 'Ramalho'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(renzo.__dict__)
 
